f8ba1236ca7e184e2c233e2e405dcf6b6a51fac7
alekskimeu <kim3ual3x@gmail.com>
2021-12-15 20:17:50 +0300

    Implement middleware

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { signOut } from "next-auth/react";
 import {
 	HomeIcon,
 	SearchIcon,
@@ -11,6 +12,13 @@ import {
 const Sidebar = () => {
 	return (
 		<div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+			<button
+				onClick={() => signOut()}
+				className="flex items-center space-x-2 hover:text-white"
+			>
+				<HomeIcon className="h-5 w-5" />
+				<p>Home</p>
+			</button>
 			<div className="space-y-4">
 				<button className="flex items-center space-x-2 hover:text-white">
 					<HomeIcon className="h-5 w-5" />
